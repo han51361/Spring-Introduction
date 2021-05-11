8bf0e12c3d8bf6b24b9644486ca02886544da8fd
xonmin <han51361@naver.com>
2021-05-11 16:00:05 +0900

    0511 스프링 웹 브라우저 데이터 처리 방식

---
+++ src/main/java/springintro/hellospring/controller/HelloController.java
@@ -0,0 +1,46 @@
+package springintro.hellospring.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "xonmin");
+        return "hello";  //rsc 의 밑에 해당 리턴값과 가틍ㄴ 파일이름에 넘겨라
+    }
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" +name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    @Getter @Setter
+    private class Hello {
+        private String name;
+
+    }
+}
